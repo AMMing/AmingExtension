e17e64761b502eefdb454f926eb10932e678422b
AMMing <y2443@163.com>
2013-09-11 14:32:46 +0800

    1.0.2.1 修复了小bug

--- AmingHelper/js/cs/all.js
+++ AmingHelper/js/cs/all.js
@@ -6,7 +6,7 @@ All = {
 		Aming.getData(this.key, function(data) {
 			All.localdata = data;
 			if (All.localdata.isShowToTop) {
-				$(window).aming_scrolltop({
+				var totop = $(window).aming_scrolltop({
 					imgurl: All.localdata.totop_bg,
 					showopacity: 1,
 					mainwidth: $(window).width() - 150
